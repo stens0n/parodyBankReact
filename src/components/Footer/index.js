71a8bf8dd135d2ccb5d0165191058075be45b517
stens0n <astenson010@gmail.com>
2022-05-14 10:43:35 -0400

    commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import {FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper, 
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink} from './FooterElements';
+
 const Footer = () => {
   return (
     <FooterContainer>
@@ -15,30 +23,27 @@ const Footer = () => {
                             <FooterLink to= '/signin'>Terms of Service</FooterLink>
                     </FooterLinkItems>
                     <FooterLinkItems>
-                        <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to= '/signin'>How it works</FooterLink>
-                            <FooterLink to= '/signin'>testimonials</FooterLink>
-                            <FooterLink to= '/signin'>careers</FooterLink>
-                            <FooterLink to= '/signin'>Investors</FooterLink>
-                            <FooterLink to= '/signin'>Terms of Service</FooterLink>
+                        <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to= '/signin'>Contact</FooterLink>
+                            <FooterLink to= '/signin'>Support</FooterLink>
+                            <FooterLink to= '/signin'>Destinations</FooterLink>
+                            <FooterLink to= '/signin'>Sponsorships</FooterLink>
                     </FooterLinkItems>
                 </FooterLinksWrapper>
                 <FooterLinksWrapper>
                     <FooterLinkItems>
-                        <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to= '/signin'>How it works</FooterLink>
-                            <FooterLink to= '/signin'>testimonials</FooterLink>
-                            <FooterLink to= '/signin'>careers</FooterLink>
-                            <FooterLink to= '/signin'>Investors</FooterLink>
-                            <FooterLink to= '/signin'>Terms of Service</FooterLink>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to= '/signin'>Submit Videos</FooterLink>
+                            <FooterLink to= '/signin'>Ambassardors</FooterLink>
+                            <FooterLink to= '/signin'>Agency</FooterLink>
+                            <FooterLink to= '/signin'>Influencer</FooterLink>
                     </FooterLinkItems>
                     <FooterLinkItems>
-                        <FooterLinkTitle>About Us</FooterLinkTitle>
-                            <FooterLink to= '/signin'>How it works</FooterLink>
-                            <FooterLink to= '/signin'>testimonials</FooterLink>
-                            <FooterLink to= '/signin'>careers</FooterLink>
-                            <FooterLink to= '/signin'>Investors</FooterLink>
-                            <FooterLink to= '/signin'>Terms of Service</FooterLink>
+                        <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to= '/signin'>Instagram</FooterLink>
+                            <FooterLink to= '/signin'>Facebook</FooterLink>
+                            <FooterLink to= '/signin'>Youtube</FooterLink>
+                            <FooterLink to= '/signin'>Twitter</FooterLink>
                     </FooterLinkItems>
                 </FooterLinksWrapper>
             </FooterLinksContainer>
