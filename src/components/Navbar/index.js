4410f96eaa119a1e18c11ff3747084a6bb2634e3
stens0n <astenson010@gmail.com>
2022-06-05 10:50:36 -0400

    scroll effect

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -33,57 +33,143 @@ import {
 
 
 const Navbar = ( { toggle } ) => {
+  
   const [scrollNav, setScrollNav] = useState(false)
   
   const changeNav = () => {
-      if(window.scrollY >= 80) {
+      
+        if(window.scrollY >= 80) {
+          
           setScrollNav(true)
+      
       } else {
+
           setScrollNav(false)
+
       }
   }
   
   
   useEffect(() => {
+
       window.addEventListener('scroll', changeNav)
+
   }, [] );
 
 
   const toggleHome = () => {
+
       scrollyfortress.scrollToTop();
+
   }
+
 
   return (
     <>
     <IconContext.Provider value={{ color: '#fff' }} />
+
       <Nav scrollNav={scrollNav}>
+
         <NavbarContainer>
+
             <NavLogo to= '/' onClick={ toggleHome }>Stenson💰</NavLogo>
+
             <MobileIcon onClick= { toggle } >
+
                 <FaBars />
+
             </MobileIcon>
+
             <NavMenu>
+
                 <NavItem>
-                    <NavLinks to= 'about' >About</NavLinks>
+
+                    <NavLinks 
+                    to= 'about' 
+                    
+                    smooth={true} 
+                    
+                    duration={500} 
+                    
+                    spy={true} 
+                    
+                    exact='true' 
+                    
+                    offset={-80}
+                    >About
+                    </NavLinks>
                 </NavItem>
+
                 <NavItem>
-                    <NavLinks to= 'discover' >Discover</NavLinks>
+
+                    <NavLinks 
+                    to= 'discover' 
+                    
+                    smooth={true} 
+                    
+                    duration={500} 
+                    
+                    spy={true} 
+                    
+                    exact='true' 
+                    
+                    offset={-80}
+                    
+                    >Discover
+                    </NavLinks>
                 </NavItem>
+
                 <NavItem>
-                    <NavLinks to= 'services' >Services</NavLinks>
+
+                    <NavLinks 
+                    to= 'services' 
+                    
+                    smooth={true} 
+                    
+                    duration={500} 
+                    
+                    spy={true} 
+                    
+                    exact='true' 
+                    
+                    offset={-80}
+                    
+                    >Services
+                    </NavLinks>
                 </NavItem>
+
                 <NavItem>
-                    <NavLinks to= 'signup' >Sign Up</NavLinks>
+
+                    <NavLinks
+                    to= 'signup' 
+                    
+                    smooth={true} 
+                    
+                    duration={500} 
+                    
+                    spy={true} 
+                    
+                    exact='true' 
+                    
+                    offset={-80} 
+                    
+                    >Sign Up
+                    </NavLinks>
                 </NavItem>
             </NavMenu>
+
             <NavBtn>
                 <NavBtnLink to= '/signin' >Sign In</NavBtnLink>
             </NavBtn>
+
         </NavbarContainer>
       </Nav>
+
     <IconContext.Provider />
     </>
   );
 };
+
+
 
 export default Navbar
